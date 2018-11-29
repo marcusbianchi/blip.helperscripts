b876905a30ddfdaa4f9b44f6ea42aae4a02888df
Marcus Bianchi <marcusb@take.net>
2018-11-29 12:02:39 -0200

    Correction on chatbase key Case sensitive

--- Modules/addchatbaseintegration.js
+++ Modules/addchatbaseintegration.js
@@ -108,11 +108,11 @@ exports.addchatbaseintegration = (function () {
 										option['label']['type'] === 'application/vnd.lime.web-link+json' && option['label']['value']['uri']) {
 										if (option['label']['value']['uri'].search("{{config.chatbasetrack}}") === -1) {
 											console.log(option['label']['value']['uri'])
-											option['label']['value']['uri'] = "{{config.chatbasetrack}}api_key={{config.chatbaseKey}}&platform={{config.platform}}&version={{config.version}}&url=" + option['label']['value']['uri']
+											option['label']['value']['uri'] = "{{config.chatbasetrack}}api_key={{config.chatbasekey}}&platform={{config.platform}}&version={{config.version}}&url=" + option['label']['value']['uri']
 										}
 										else{
 											url = option['label']['value']['uri'].substring(option['label']['value']['uri'].search("http"))
-											option['label']['value']['uri'] = "{{config.chatbasetrack}}api_key={{config.chatbaseKey}}&platform={{config.platform}}&version={{config.version}}&url=" +url
+											option['label']['value']['uri'] = "{{config.chatbasetrack}}api_key={{config.chatbasekey}}&platform={{config.platform}}&version={{config.version}}&url=" +url
 										}
 									}
 								}
